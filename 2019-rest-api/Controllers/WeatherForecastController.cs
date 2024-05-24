7187be09951cdee119cf15711e79cb477c7fa386
Vic <vandoorne1978@gmail.com>
2024-05-24 12:53:34 -0500

    update namespace in weather forecast controller.

--- 2019-rest-api/Controllers/WeatherForecastController.cs
+++ 2019-rest-api/Controllers/WeatherForecastController.cs
@@ -3,9 +3,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
-namespace _2019_rest_api.Controllers
+namespace _2019_rest_api
 {
     [ApiController]
     [Route("[controller]")]
